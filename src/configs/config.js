9cbfe0d321161b8d790710d9c7e269f8e2927050
Fauzaan <123953439+Fauzaan-Contrivers@users.noreply.github.com>
2023-05-05 16:16:53 +0500

    Update config.js

--- src/configs/config.js
+++ src/configs/config.js
@@ -2,6 +2,6 @@
 // export const BASE_URL = 'https://backend.soaanz.com/sarfez_api/dashboard/'
 
 // LOCAL
-export const BASE_URL = 'http://localhost:5000/'
+export const BASE_URL = 'http://52.70.96.156:8080/'
 
 export const API_KEY = 'dd5-4salkdufhlaisfd-akjhiud4-d0c228b05cb4'
