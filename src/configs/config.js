81d3a351397e6e4e08f22a7f3fd6a49b2660a756
Fauzaan <46840205+FauzaanSheikh@users.noreply.github.com>
2023-05-28 00:07:33 +0500

    Update config.js

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://54.172.17.47:3001/";
+export const BASE_URL = "https://backend.samodrei.com/";
 // export const BASE_URL = "http://localhost:5000/";
 export const API_KEY = "dd5-4salkdufhlaisfd-akjhiud4-d0c228b05cb4";
