8700dd247e04073f5f0a6396571ed455a6d289ce
Fauzaan <fauzaan@hemavi.com>
2023-08-17 14:19:20 +0500

    Update CI/CD

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,3 +1,3 @@
 export const BASE_URL = "https://backend.samodrei.com/";
-// export const BASE_URL = "http://localhost:5000/";
+// export const BASE_URL = "http://localhost:3001/";
 export const API_KEY = "dd5-4salkdufhlaisfd-akjhiud4-d0c228b05cb4";
