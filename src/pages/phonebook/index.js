01c027cc097152bda19c559a8b440668c3e333d0
mirzaabubakr1 <121239944+mirzaabubakr1@users.noreply.github.com>
2023-09-05 17:21:14 +0500

    Updated Refectered Phonebook Index

--- src/pages/phonebook/index.js
+++ src/pages/phonebook/index.js
@@ -13,8 +13,9 @@ import {
   fetchPrescribersforPhoneLogs,
   updateDisabledPrescriber,
   onPrescriberFilterChangeHandler,
-  onCallLogFilterChangeHandler,
-} from "src/store";
+} from "src/store/prescribers";
+
+import { onCallLogFilterChangeHandler } from "src/store/call_logs";
 
 // ** MUI Imports
 import Grid from "@mui/material/Grid";
@@ -41,7 +42,10 @@ const PhoneBook = () => {
   const { socket } = store.call_logs.filter;
   const userData = JSON.parse(window.localStorage.getItem(authConfig.userData));
 
-  useEffect(() => fetchPrescribersOnUpdate(), [store.prescribers.filter.page, store.prescribers.filter.pageSize]);
+  useEffect(
+    () => fetchPrescribersOnUpdate(),
+    [store.prescribers.filter.page, store.prescribers.filter.pageSize]
+  );
   useEffect(() => setupRingCentralScript(), []);
   useEffect(() => initializeSocket(), []);
   useEffect(() => configureSocketEvents(socket), [socket]);
@@ -49,11 +53,12 @@ const PhoneBook = () => {
   const fetchPrescribersOnUpdate = () => {
     setIsLoading(true);
     const fetchPrescribersDataWithDebounce = debounce(() => {
-      dispatch(fetchPrescribersforPhoneLogs({
-        page_num: store.prescribers.filter.page + 1,
-        page_size: store.prescribers.filter.pageSize,
-      }))
-      .then(() => {
+      dispatch(
+        fetchPrescribersforPhoneLogs({
+          page_num: store.prescribers.filter.page + 1,
+          page_size: store.prescribers.filter.pageSize,
+        })
+      ).then(() => {
         setPage(store.prescribers.filter.page);
         setIsLoading(false);
       });
@@ -65,7 +70,8 @@ const PhoneBook = () => {
 
   const setupRingCentralScript = () => {
     const rcs = document.createElement("script");
-    rcs.src = "https://ringcentral.github.io/ringcentral-embeddable/adapter.js";
+    rcs.src =
+      "https://ringcentral.github.io/ringcentral-embeddable/adapter.js?newAdapterUI=1";
     rcs.onload = () => {
       RCAdapter.setClosed(true);
       console.log("Script has been loaded successfully!");
@@ -77,7 +83,12 @@ const PhoneBook = () => {
 
   const initializeSocket = () => {
     const newSocket = io.connect(BASE_URL, { transports: ["websocket"] });
-    dispatch(onCallLogFilterChangeHandler({ filter: "socket", value: newSocket }));
+    dispatch(
+      onCallLogFilterChangeHandler({
+        filter: "socket",
+        value: newSocket,
+      })
+    );
   };
 
   const configureSocketEvents = (socket) => {
@@ -94,11 +105,14 @@ const PhoneBook = () => {
 
   const updatePrescriberCallStatus = async (prescriberId, flag) => {
     try {
-      const response = await fetch(`${BASE_URL}tele-prescribers/update_tele_prescriber_call_status`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ prescriberId, flagged: flag }),
-      });
+      const response = await fetch(
+        `${BASE_URL}tele-prescribers/update_tele_prescriber_call_status`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ prescriberId, flagged: flag }),
+        }
+      );
       console.log("DATA", await response.json());
     } catch (error) {
       console.error("CHECK", error);
@@ -106,28 +120,148 @@ const PhoneBook = () => {
   };
 
   const onActionClick = (prescriberId) => socket.emit("message", prescriberId);
-  const isActionDisabled = (prescriberId) => store.prescribers.disabledPrescribers[prescriberId];
+  const isActionDisabled = (prescriberId) =>
+    store.prescribers.disabledPrescribers[prescriberId];
+
+  const defineColumns = () => {
+    const defaultColumns = [
+      {
+        field: "Id",
+        minWidth: 210,
+        headerName: "Id",
+        renderCell: ({ row }) => (
+          <Typography variant="body2">{`${row?.Id}`}</Typography>
+        ),
+      },
+      {
+        field: "NPI",
+        minWidth: 210,
+        headerName: "NPI",
+        renderCell: ({ row }) => (
+          <Typography variant="body2">{`${row?.NPI}`}</Typography>
+        ),
+      },
+      {
+        field: "Name",
+        minWidth: 210,
+        headerName: "Name",
+        renderCell: ({ row }) => (
+          <Typography variant="body2">{`${row?.First_Name} ${row?.Last_Name}`}</Typography>
+        ),
+      },
+      {
+        field: "Phone",
+        minWidth: 210,
+        headerName: "Phone",
+        renderCell: ({ row }) => (
+          <Typography variant="body2">{`${row?.Phone}`}</Typography>
+        ),
+      },
+      {
+        field: "Fax",
+        minWidth: 120,
+        headerName: "FAX",
+        renderCell: ({ row }) => (
+          <Typography variant="body2">{`${row?.Fax}`}</Typography>
+        ),
+      },
+      {
+        field: "MeetingDate",
+        minWidth: 210,
+        headerName: "Meeting Date",
+        renderCell: ({ row }) => (
+          <Typography variant="body2">
+            {row?.MeetingDate
+              ? moment(row.MeetingDate).local().format("YYYY-MM-DD HH:mm:ss")
+              : ""}
+          </Typography>
+        ),
+      },
+      {
+        flex: 0.1,
+        minWidth: 100,
+        field: "Action",
+        headerName: "Action",
+        renderCell: ({ row }) => (
+          <Grid container alignItems="center">
+            <Link href={`/phonebook/preview/${row.Id}`} passHref>
+              <IconButton
+                size="small"
+                component="a"
+                sx={{ textDecoration: "none", cursor: "pointer" }}
+                onClick={() => onActionClick(row.Id)}
+                disabled={
+                  isActionDisabled(row.Id) || row.isOnCall ? true : false
+                }
+              >
+                <EyeOutline
+                  fontSize="small"
+                  sx={{
+                    color:
+                      isActionDisabled(row.Id) || row.isOnCall ? "red" : null,
+                  }}
+                />
+              </IconButton>
+            </Link>
+          </Grid>
+        ),
+      },
+    ];
+    return [...defaultColumns];
+  };
 
   const columns = defineColumns();
 
+  const pageSizeChangeHandler = (newPageSize) => {
+    dispatch(
+      onPrescriberFilterChangeHandler({
+        filter: "pageSize",
+        value: newPageSize,
+      })
+    );
+  };
+
+  const pageChangeHandler = (newPage) => {
+    dispatch(
+      onPrescriberFilterChangeHandler({
+        filter: "page",
+        value: newPage,
+      })
+    );
+  };
+
   return (
     <div>
-      {ability?.can("read", "acl-page") && (
-        <Grid item xs={12}>
-          <Card>
-            <DataGrid
-              // ... other props
-            />
-          </Card>
-        </Grid>
-      )}
+      {ability?.can("read", "acl-page") ? (
+        <>
+          <Grid item xs={12}>
+            <Card>
+              <DataGrid
+                autoHeight
+                pagination
+                rows={
+                  isLoading ? [] : store.prescribers.PhonebookPrescribersData
+                }
+                columns={columns}
+                loading={isLoading}
+                getRowId={(row) => row?.Id}
+                rowCount={store.prescribers.totalRecords}
+                disableSelectionOnClick
+                page={page}
+                pageSize={store.prescribers.filter.pageSize}
+                rowsPerPageOptions={[20, 30, 50]}
+                onPageChange={(newPage) => pageChangeHandler(newPage)}
+                onPageSizeChange={(newPageSize) =>
+                  pageSizeChangeHandler(newPageSize)
+                }
+                paginationMode="server"
+              />
+            </Card>
+          </Grid>
+        </>
+      ) : null}
     </div>
   );
-};
-
-const defineColumns = () => {
-  // ... your columns definition
-  return [...defaultColumns];
 };
 
 PhoneBook.acl = {
